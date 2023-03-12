70c3729ca960ec67504951e447bea401246474d1
Brandon <brandonkly@ucla.edu>
2023-03-11 18:08:33 -0800

    search bar typing and city list

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,33 +1,91 @@
 // https://developers.google.com/maps/documentation/javascript/places#place_searches
 
-import React, {useState} from "react";
+import React, { MouseEventHandler, useState } from "react";
+
+type City = {
+  address: string;
+  coords: {
+    lat: number;
+    lon: number;
+  };
+  place_id: string;
+};
+
+// address: cityData.results[0].formatted_address,
+// coords: {
+//   lat: cityData.results[0].geometry.location.lat,
+//   lon: cityData.results[0].geometry.location.lon,
+// },
+// place_id: cityData.results[0].place_id,
+
+type GeoLocation = {
+  formatted_address: string;
+  geometry: {
+    location: {
+      lat: number;
+      lon: number;
+    };
+  };
+  place_id: string;
+};
+
+type GeoLocationResult = {
+  results: GeoLocation[];
+};
 
 
 export default function AddressSearch() {
-    const [userAddress, setUserAddress] = useState('');
+  const cityList: City[] = []
 
-    const handleSetUserAddress = (event: any) => {
-        const newAddress = event.target.value
-        console.log('address ' + newAddress)
-        setUserAddress(newAddress)
-    }
+  const [userAddress, setUserAddress] = useState<string>("");
 
-    const handleFormSubmit = (event: any) => {
-        getCoords(userAddress).then((result) => {
-            event.preventDefault();
-            console.log(result)
-            // return result.json();
-            // alert(`${apiFetch(result)}`);
-        })
-      };
+  const handleSetUserAddress = (event: any) => {
+    const newAddress = event.target.value;
+    console.log("address " + newAddress);
+    setUserAddress(newAddress);
+  };
 
-    function getCoords(userAddress: string) {
-        let requestUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${userAddress}&key=${import.meta.env.VITE_APIKEY}`;
-        console.log('fetching')
-        return fetch(requestUrl).then((result) => result.json())
-      }
+  const handleFormSubmit: MouseEventHandler = (event) => {
+    getCoords(userAddress).then((result) => {
+      event.preventDefault()
+      console.log(result);
+      // return result.json();
+      // alert(`${apiFetch(result)}`);
+    });
+  };
 
-    return <>
+  async function getCoords(userAddress: string) {
+    let requestUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${userAddress}&key=${
+      import.meta.env.VITE_APIKEY
+    }`;
+    let res = await fetch(requestUrl);
+    const cityData = (await res.json()) as GeoLocationResult;
+
+    let city: City = {
+      address: cityData.results[0].formatted_address,
+      coords: {
+        lat: cityData.results[0].geometry.location.lat,
+        lon: cityData.results[0].geometry.location.lon,
+      },
+      place_id: cityData.results[0].place_id,
+    };
+
+    // cityList.push(city)
+    // return city
+
+    // return cityData.json()
+
+    // try {
+    //   let coords = cityData.json();
+    //   return coords
+    // } catch (error) {
+    //   console.error(error);
+    // }
+    // console.log('fetching')
+  }
+
+  return (
+    <>
       <form className="form">
         <input
           value={userAddress}
@@ -41,4 +99,5 @@ export default function AddressSearch() {
         </button>
       </form>
     </>
+  );
 }
