42a95eb95aff19b1757332ae89f8c7e710aa2a23
MJ <mirjanadukicc@gmail.com>
2023-03-13 20:29:14 -0700

    wip mijiana

--- server/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const courseRoutes = require('./courseRoutes');
-const studentRoutes = require('./studentRoutes');
-
-router.use('/courses', courseRoutes);
-router.use('/students', studentRoutes);
-
-module.exports = router;
