b19cb800df0fa83049796a41e297de1717278554
MJ <mirjanadukicc@gmail.com>
2023-03-16 13:35:16 -0700

    figured out spacing, decent for now, future style title

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -51,49 +51,49 @@ export default function Signup() {
         className="card-body flex justify-center p-10 m-5"
         onSubmit={handleFormSubmit}
       >
-        <div className="form-outline space-y-4 rounded-lg">
-          <div className="font-bold text-lg justify-center">
+        <div className="form-outline space-y-4 rounded-lg tracking-wide">
+          <div className="font-bold text-lg justify-center font-semibold tracking-wider">
           <h2>REGISTER: </h2>
           </div>
           <label htmlFor="username-input-signup" className="form-label">
-            Name:
+            name:
           </label>
           <input
             onChange={handleFormChange}
             name="name"
             type="text"
             id="username-input-signup"
-            className="form-control"
+            className="form-control rounded-lg"
           />
           <div className="">
           <label htmlFor="username-input-signup" className="form-label">
-            Email:
+            email:
           </label>
           <input
             onChange={handleFormChange}
             name="email"
             type="text"
             id="username-input-signup"
-            className="form-control"
+            className="form-control rounded-lg"
           />
           </div>
           <div>
-          <label htmlFor="password-input-signup" className="form-label">
-            Password:
+          <label htmlFor="password-input-signup" className="form-label ">
+            password:
           </label>
           <input
             onChange={handleFormChange}
             name="password"
             type="password"
             id="password-input-signup"
-            className="form-control"
+            className="form-control rounded-lg w-3/5"
           />
           </div>
           <button
             type="submit"
-            className="btn btn-primary px-4 py-2 pr-4 pl-4 text-gray-600 hover:text-gray-700 hover:bg-gray-50 shadow-sm appearance-none m-5 rounded-md bg-white"
+            className="btn btn-primary pl-4 pr-4 px-2 py-2 text-gray-600 hover:text-gray-700 hover:bg-gray-50 shadow-sm appearance-none rounded-md bg-white"
           >
-            Signup!
+            signup!
           </button>
         </div>
       </form>
