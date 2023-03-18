7b6b61864e8156414bf6cb6bcbb0b45535b2ed8b
Lsenn1 <lsenn@lion.lmu.edu>
2023-03-18 16:46:37 -0700

    searching will now add the address and a place to the database, need to build out this functionality more

--- server/models/Address.js
+++ server/models/Address.js
@@ -1,27 +1,29 @@
 const { Schema, Types, model } = require("mongoose");
 
 const addressSchema = new Schema({
-    address: String,
-    coords: {
-        lat: Number,
-        lng: Number,
-    },
-    place_id: String,
-    // results: [{
-    //   results_id: Schema.Types.ObjectId,
-    //   ref: "place"
-    // }]
-    //comments: [{}]
+  address: String,
+  coords: {
+    lat: Number,
+    lng: Number,
+  },
+  place_id: String,
+  // results: [{
+  //   results_id: Schema.Types.ObjectId,
+  //   ref: "place"
+  // }]
+  //comments: [{}]
 });
-
 
 // Search: {
 //   formatted origin address:
-//     Places: 
+//     Places:
 //       distances, routes
 //       type
 
 // }
 
 const Address = model("address", addressSchema);
+Address.createCollection().then(function (collection) {
+  console.log("Address Collection is created!");
+});
 module.exports = Address;
