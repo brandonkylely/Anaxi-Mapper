fdf4e9ecaa9a2f865e233ca9b651845d83e0f34c
Lsenn1 <lsenn@lion.lmu.edu>
2023-03-19 16:36:50 -0700

    current search rendering changes

--- client/src/components/CurrentSearch.tsx
+++ client/src/components/CurrentSearch.tsx
@@ -13,21 +13,18 @@ export default function CurrentSearch() {
         <>
     <div className="columns-2 flex">      
             <div className="w-full m-4 bg-white border border-gray-200 rounded-lg shadow p-4">
-                
-                <Favorite />
+                <h2>Current Search Component!</h2>
+                <h2>{address}</h2>
                 
                 
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Lorem Ipsum</h5>
                 
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc finibus venenatis odio, commodo laoreet ex blandit eu. </p>
                 
-                
-                <CommentsList />
-                <Comments />
+
 
             </div>
 
-            <FavoriteList />
             
     </div>
         </>
