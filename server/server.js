c269b0543d1f8e285d5e771d2a0db7e864619ea7
Lsenn1 <lsenn@lion.lmu.edu>
2023-03-21 08:44:33 -0700

    set up database with atlas, need to refactor

--- server/server.js
+++ server/server.js
@@ -1,9 +1,46 @@
 require("dotenv").config();
 const express = require("express");
-const db = require("./config/connection");
+const mongoose = require("mongoose");
+// const db = require("./config/connection");
 const routes = require("./controllers");
 const auth = require("./middleware/auth");
 // require('./src')();
+
+const database = module.exports = () => {
+  const connectionParams = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    // useUnifiedTopology: true,
+  };
+
+  console.log(`${process.env.DB}`);
+  try {
+    mongoose.connect(`mongodb+srv://Anaxi-Mapper:${process.env.DB}@cluster0.aov6xcm.mongodb.net/?retryWrites=true&w=majority`)
+  }
+  catch (err) {
+    console.log(err)
+  }
+}
+
+database();
+
+  // connect(connectionString, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  // });
+  
+  // module.exports = connection;
+  
+  // const { MongoClient, ServerApiVersion } = require('mongodb');
+  // const uri = ;
+  // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  // client.connect(err => {
+  //   const collection = client.db("test").collection("devices");
+  //   // perform actions on the collection object
+  //   client.close();
+  // });
+
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -28,8 +65,9 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-db.once("open", () => {
+// db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running http://localhost:${PORT}`);
   });
-});
+//   });
+// });
