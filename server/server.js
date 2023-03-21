466520cafb0ab07dfc54090674e9e59525385c1c
Lsenn1 <lsenn@lion.lmu.edu>
2023-03-21 12:57:12 -0700

    hopefully fixed db causing heroku crash on deployment

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const db = require("./config/connection");
 const routes = require("./controllers");
 const auth = require("./middleware/auth");
+
+
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -11,23 +14,14 @@ app.use(express.json());
 app.use(auth);
 app.use(routes);
 
-app.use("/api/favorite", require("./controllers/api/favorite"));
-app.use("/api/comment", require("./controllers/api/comment"));
+app.use('/api/favorite', require('./controllers/api/favorite'));
+app.use('/api/comment', require('./controllers/api/comment'));
 
-// test route
-app.get("/api/test", (req, res) => {
-  console.log("test route hit");
-  res.json({ message: "Hello from server!" });
-});
+app.use(routes);
 
-if (process.env.NODE_ENV === "production") {
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-  });
-}
-
-db.once("open", () => {
+// db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running http://localhost:${PORT}`);
   });
-});
+//   });
+// });
