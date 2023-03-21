81cdb208d2fd8429c0fc2839e95412895b610c3a
Lsenn1 <lsenn@lion.lmu.edu>
2023-03-21 13:23:58 -0700

    update

--- server/server.js
+++ server/server.js
@@ -7,13 +7,14 @@ const auth = require("./middleware/auth");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static("../client/dist"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(auth);
 app.use(routes);
 
-app.use('/api/favorite', require('./controllers/api/favorite'));
-app.use('/api/comment', require('./controllers/api/comment'));
+app.use("/api/favorite", require("./controllers/api/favorite"));
+app.use("/api/comment", require("./controllers/api/comment"));
 
 // test route
 app.get("/api/test", (req, res) => {
@@ -21,10 +22,12 @@ app.get("/api/test", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running http://localhost:${PORT}`);
   });
-  });
-
+});
