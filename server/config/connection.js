a9ad2fddf77ba28fa7ba5b3aa5fe0bc02670698d
Lsenn1 <lsenn@lion.lmu.edu>
2023-03-21 08:53:57 -0700

    refactored atlas connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,25 @@
-// const { connect, connection } = require('mongoose');
+require("dotenv").config();
+const { connect, connection } = require("mongoose");
+
+const database = (module.exports = () => {
+  const connectionParams = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    // useUnifiedTopology: true,
+  };
+
+  try {
+    connect(
+      `mongodb+srv://Anaxi-Mapper:${process.env.DB}@cluster0.aov6xcm.mongodb.net/?retryWrites=true&w=majority`
+    );
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+database();
+
+module.exports = connection;
 
 // // After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
 // // Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
@@ -9,5 +30,3 @@
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 // });
-
-// module.exports = connection;
--- server/server.js
+++ server/server.js
@@ -1,46 +1,8 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
-// const db = require("./config/connection");
+const db = require("./config/connection");
 const routes = require("./controllers");
 const auth = require("./middleware/auth");
-// require('./src')();
-
-const database = module.exports = () => {
-  const connectionParams = {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    // useUnifiedTopology: true,
-  };
-
-  try {
-    mongoose.connect(`mongodb+srv://Anaxi-Mapper:${process.env.DB}@cluster0.aov6xcm.mongodb.net/?retryWrites=true&w=majority`)
-  }
-  catch (err) {
-    console.log(err)
-  }
-}
-
-database();
-
-  // connect(connectionString, {
-  //   useNewUrlParser: true,
-  //   useUnifiedTopology: true,
-  // });
-  
-  // module.exports = connection;
-  
-  // const { MongoClient, ServerApiVersion } = require('mongodb');
-  // const uri = ;
-  // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-  // client.connect(err => {
-  //   const collection = client.db("test").collection("devices");
-  //   // perform actions on the collection object
-  //   client.close();
-  // });
-
-
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -49,8 +11,8 @@ app.use(express.json());
 app.use(auth);
 app.use(routes);
 
-app.use('/api/favorite', require('./controllers/api/favorite'));
-app.use('/api/comment', require('./controllers/api/comment'));
+app.use("/api/favorite", require("./controllers/api/favorite"));
+app.use("/api/comment", require("./controllers/api/comment"));
 
 // test route
 app.get("/api/test", (req, res) => {
@@ -64,9 +26,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// db.once("open", () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running http://localhost:${PORT}`);
   });
-//   });
-// });
+});
