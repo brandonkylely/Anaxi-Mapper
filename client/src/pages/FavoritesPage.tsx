5a14db8e4e697429f49927b382cda5d138901a9b
Lsenn1 <lsenn@lion.lmu.edu>
2023-03-26 23:53:27 -0700

    map through all favorite searches now, added placeholder image

--- client/src/pages/FavoritesPage.tsx
+++ client/src/pages/FavoritesPage.tsx
@@ -20,7 +20,7 @@ export default function SearchList() {
     axios.get(`/api/favorite/getFavoritePlaces/${userId}`).then((response) => {
       if (response.data.success) {
         console.log(response.data.favorites, "response");
-        setFavoriteValue(response.data.favorites[0].search); //right now this is only being set to a single search result from favorites, need to refactor code to take the entire favorites
+        setFavoriteValue(response.data.favorites); //right now this is only being set to a single search result from favorites, need to refactor code to take the entire favorites
         setLocalLoader(true);
       } else {
         alert("Failed to get list");
@@ -37,7 +37,21 @@ export default function SearchList() {
           <div className="font-righteous m-4 bg-white border border-gray-200 rounded-lg shadow p-4">
             <h1 className="font-bold">{search.name}</h1>
             <p>{search.vicinity}</p>
-            <img src={search.photos[0].photo_reference} alt={search.name} />
+            {search.photos ? (
+              <img
+                src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${
+                  search.photos[0].photo_reference
+                }&key=${import.meta.env.VITE_APIKEY}`}
+                alt="restaurant"
+                className="h-56"
+              />
+            ) : (
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
+                alt="No Image Available"
+                className="h-56"
+              />
+            )}
             <p>Rating: {search.rating}</p>
             <p>Price Level: {search.price_level}</p>
           </div>
@@ -46,22 +60,6 @@ export default function SearchList() {
     );
   }
 
-  //searches structure
-  [
-    {
-      //this object would be favoriteValue[0]
-      coords: {
-        //coords of origin point
-        lat: 33.7174708,
-        lng: -117.8311428,
-      },
-      search: [], //array of search results --> this will make up the main body of a single favorited item, everything else will be a header
-      address: "Orange County, CA, USA", //address of origin point
-      radius: 20000, //radius of search
-      type: "restaurant", //type parameter of search
-      _id: "641a32cfe2532a6abae760c1",
-    },
-  ];
   function SearchList({ searches }) {
     //this is the main body of the page
     return (
@@ -70,9 +68,18 @@ export default function SearchList() {
           Favorite Searches
         </h6>
         <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-          {favoriteValue.map((search, index) => (
+          {favoriteValue.map((favorite, index) => (
             <div>
-              <SearchItem key={index} search={search} />
+              {/* <button className="accordionButton"> */}
+              <h1>{favorite.address}</h1>
+              <h1>{favorite.radius}</h1>
+              <h1>{favorite.type}</h1>
+              {/* </button> */}
+              {/* <div className="accordion-body"> */}
+              {favoriteValue[index].search.map((search, index) => (
+                <SearchItem key={index} search={search} />
+              ))}
+              {/* </div> */}
             </div>
           ))}
         </ul>
